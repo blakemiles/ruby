a140a8afad0a2e6441dbc8484b2a771adbe71cb2
blakemiles <blakemiles84@gmail.com>
2016-03-09 09:39:36 -0500

    added ruby modules 1-3

---
+++ studio_game/studio_game.rb
@@ -0,0 +1,13 @@
+name1 = 'larry'
+health1 = 60
+puts name1 + '\'s' + ' health is ' + health1.to_s
+puts "#{name1}\'s health is #{health1}"
+puts "#{name1}\'s health is #{health1 * 3}"
+puts "#{name1}\'s health is #{health1 / 9}"
+puts "#{name1}\'s health is #{health1 / 9.0}"
+
+#part2
+puts "Players: \n \t larry \n \t curly \n \t moe"
+name2 = 'curly'
+name3 = 'moe'
+puts "Players: \n \t #{name1} \n \t #{name2}\n \t #{name3}"
